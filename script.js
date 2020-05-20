ef7b90eb65eb225a7643c95cd97e3bae292f00dd
Eklavya Sharma <ekurgn@gmail.com>
2020-05-20 19:46:28 +0530

    use nfdh to pack inventory

--- script.js
+++ script.js
@@ -229,6 +229,56 @@ function nextFitStrip(items, binXLen) {
     return [ySum, sol];
 }
 
+function keyFuncToCompareFunc(keyFunc) {
+    return function(x, y) {
+        let xKey = keyFunc(x), yKey = keyFunc(y);
+        if(xKey < yKey) {
+            return -1;
+        }
+        else if(xKey > yKey) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    };
+}
+
+function sortMaps(a, compareFunc) {
+// returns (pi, piinv, sorted(a)) such that a[pi] = sorted(a)
+    var b = [], n = a.length;
+    for(var i=0; i<n; ++i) {
+        b.push([i, a[i]]);
+    }
+    b.sort(function(x, y) {
+        return compareFunc(x[1], y[1]);
+    });
+    var pi = [], sortedA = [], piinv = Array(n);
+    for(var i=0; i<n; ++i) {
+        pi.push(b[i][0]);
+        sortedA.push(b[i][1]);
+        piinv[b[i][0]] = i;
+    }
+    b.length = 0;
+    return [pi, piinv, sortedA];
+}
+
+function applyIndexMap(a, pi) {
+    var b = [], n = a.length;
+    for(var i=0; i<n; ++i) {
+        b.push(a[pi[i]]);
+    }
+    return b;
+}
+
+function nfdhStrip(items, binXLen) {
+    var compareFunc = keyFuncToCompareFunc(function(x) {return -x.yLen;});
+    let [pi, piinv, itemsSorted] = sortMaps(items, compareFunc);
+    let [ySum, solSorted] = nextFitStrip(itemsSorted, binXLen);
+    var sol = applyIndexMap(solSorted, piinv);
+    return [ySum, sol];
+}
+
 //==[ IO Layer ]================================================================
 
 class InputError extends Error {
@@ -497,7 +547,7 @@ class Game {
             maxXLen = Math.max(maxXLen, item.xLen);
         }
         var invXLen = maxXLen;
-        let [invYLen, nextFitSol] = nextFitStrip(inputItems, maxXLen);
+        let [invYLen, nextFitSol] = nfdhStrip(inputItems, maxXLen);
         this.nextFitSol = nextFitSol;
 
         if(scaleFactor === null) {
